4518eaf4abfefd36b41ee569800ea1c2d53a0422
David Munger <mungerd@gmail.com>
2013-05-21 12:04:18 -0400

    fix PATH variable manipulation in web interface

--- latbuilder-web/bin/LatBuilderWeb.py
+++ latbuilder-web/bin/LatBuilderWeb.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python2
 
-import sys, os
+import os
 import subprocess
 
 if __name__ == '__main__':
     here = os.path.dirname(os.path.abspath(__file__))
     serve = os.path.join(os.path.dirname(here), 'share', 'latbuilder-web', 'serve.py')
-    sys.path.insert(0, here)
+    path = here + os.pathsep + os.environ.get('PATH', os.defpath)
+    os.environ['PATH'] = path
     subprocess.call(serve, shell=True)
