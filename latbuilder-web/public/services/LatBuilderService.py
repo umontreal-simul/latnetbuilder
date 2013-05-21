11a0beb0bd8e5ae0bc33a05ca113d93116c54fbc
David Munger <mungerd@gmail.com>
2013-05-21 19:52:05 -0400

    web: new service methods for weight expressions

--- latbuilder-web/public/services/LatBuilderService.py
+++ latbuilder-web/public/services/LatBuilderService.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2
 
 from jsonrpc import handleCGI, ServiceMethod
+import parsemath
 import latbuilder
 import subprocess
 
@@ -13,6 +14,14 @@ def execute(*args):
         return "ERROR:\ncommand: " + ' '.join(e.cmd) + "\nouput: " + e.output
     except OSError, e:
         return "ERROR:\ncannot execute the `latbuilder' program: " + e.strerror
+
+@ServiceMethod
+def make_product_weights(expr, dimension):
+    return [parsemath.eval(expr, {'j': j + 1}) for j in range(dimension)]
+
+@ServiceMethod
+def make_order_weights(expr, dimension):
+    return [parsemath.eval(expr, {'k': k + 1}) for k in range(dimension)]
 
 if __name__ == "__main__":
     handleCGI()
