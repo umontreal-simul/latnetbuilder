b6e92867918cda2537cd70957f25a5505bbc3663
David Munger <mungerd@gmail.com>
2013-05-23 19:04:33 -0400

    web: fixed server for windows support

--- latbuilder-web/public/serve.py
+++ latbuilder-web/public/serve.py
@@ -29,7 +29,10 @@ import webbrowser
 from multiprocessing import Process, Queue
 
 def serve(port, q):
+    CGIHTTPRequestHandler.cgi_directories = ['/services']
     try:
+        Handler = CGIHTTPRequestHandler
+        Handler.protocol_version = 'HTTP/1.0'
         httpd = HTTPServer(('localhost', port), Handler)
         address, port = httpd.socket.getsockname()
         url = 'http://{}:{}/'.format(address, port)
@@ -39,11 +42,7 @@ def serve(port, q):
         q.put(None)
 
 if __name__ == '__main__':
-    CGIHTTPRequestHandler.cgi_directories = ['/services']
     os.chdir(os.path.dirname(__file__))
-
-    Handler = CGIHTTPRequestHandler
-    Handler.protocol_version = 'HTTP/1.0'
 
     for port in range(8080, 8080 + 20):
         q = Queue()
