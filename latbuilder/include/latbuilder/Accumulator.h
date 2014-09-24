5d2c313f99486a07337e009b95fe8d48e15b9ec4
David Munger <mungerd@gmail.com>
2014-09-24 14:37:30 -0400

    improved documentation of Accumulator

--- latbuilder/include/latbuilder/Accumulator.h
+++ latbuilder/include/latbuilder/Accumulator.h
@@ -25,39 +25,70 @@
 namespace LatBuilder {
 
 /**
- * Accumulator template.
+ * Accumulator template class.
  *
- * \tparam OP              Scalar binary operator.
- *                         For example, Functor::Sum or Functor::Max.
+ * An accumulator holds a value that can be updated by feeding it with new
+ * values with the accumulate() member function.  The scalar binary operation
+ * by which new values are combined with the current value is specified with
+ * the OP template parameter.  If the values are vectorial (indexable with the
+ * [] operator), the operator OP is applied to each element of the vector
+ * through the LatBuilder::Vectorize class template.
+ *
+ * \tparam OP              Class template that takes that provides the
+ *                         implementation of a scalar binary operator.  Must
+ *                         implement the static member functions \c name(),
+ *                         that returns a string that identifies the operator,
+ *                         and \c apply(current_accumulator_value,new_value),
+ *                         which returns the new accumulator value when a value
+ *                         \c new_value is fed to the accumulator while the
+ *                         current accumulator value is
+ *                         \c current_accumulator_value.
+ *                         For examples, see Functor::Sum::name() and
+ *                         Functor::Sum::apply().
+ * \tparam VAL             Type of the values that are fed to the accumulator.
+ * \tparam ACCVAL          Type of the accumulator value (in most cases it
+ *                         should be the same as VAL).
+ *
+ * \see Functor::Sum Functor::Max
  */
-template <template <typename> class OP, typename VAL>
+template <template <typename> class OP, typename VAL, typename ACCVAL=VAL>
 class Accumulator {
 public:
 
-   Accumulator(VAL initialValue):
+   /**
+    * Type of the values fed to the accumulator.
+    */
+   typedef VAL value_type;
+
+   /**
+    * Type of the accumulator value.
+    */
+   typedef ACCVAL accumulator_value_type;
+
+   Accumulator(accumulator_value_type initialValue):
       m_value(initialValue)
    {}
 
    /**
     * Feeds a new value to the accumulator.
     */
-   void accumulate(const VAL& value)
+   void accumulate(const value_type& value)
    { accumulate(1.0, value); }
 
    /**
     * Feeds \c value multiplied by \c weight to the accumulator.
     * \c weight is assumed to already be raised at the power \c power.
     */
-   void accumulate(Real weight, const VAL& value, Real power = 1.0)
+   void accumulate(Real weight, const value_type& value, Real power = 1.0)
    { m_value = Vectorize::apply<OP<Real>>(m_value, weight * Vectorize::apply<Functor::Pow>(value, power)); }
 
    /**
     * Returns the current value of the accumulator.
     */
-   const VAL& value() const
+   const value_type& value() const
    { return m_value; }
 
-   VAL& value()
+   accumulator_value_type& value()
    { return m_value; }
 
    /**
@@ -67,7 +98,7 @@ public:
    { return OP<Real>::name(); }
 
 private:
-   VAL m_value;
+   accumulator_value_type m_value;
 };
 
 }
