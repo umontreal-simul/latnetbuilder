e596818b7548ce23b01f77a7cfec7ff71b79c3c4
David Munger <mungerd@gmail.com>
2014-10-17 15:22:13 -0400

    web-ui: added [from results] to evaluation method

--- web-ui/share/latbuilder/web-ui/js/latbuilder.js
+++ web-ui/share/latbuilder/web-ui/js/latbuilder.js
@@ -19,6 +19,96 @@ if (!String.prototype.trim) {
     }
 }
 
+// Production steps of ECMA-262, Edition 5, 15.4.4.19
+// Reference: http://es5.github.io/#x15.4.4.19
+if (!Array.prototype.map) {
+
+  Array.prototype.map = function(callback, thisArg) {
+
+    var T, A, k;
+
+    if (this == null) {
+      throw new TypeError(' this is null or not defined');
+    }
+
+    // 1. Let O be the result of calling ToObject passing the |this| 
+    //    value as the argument.
+    var O = Object(this);
+
+    // 2. Let lenValue be the result of calling the Get internal 
+    //    method of O with the argument "length".
+    // 3. Let len be ToUint32(lenValue).
+    var len = O.length >>> 0;
+
+    // 4. If IsCallable(callback) is false, throw a TypeError exception.
+    // See: http://es5.github.com/#x9.11
+    if (typeof callback !== 'function') {
+      throw new TypeError(callback + ' is not a function');
+    }
+
+    // 5. If thisArg was supplied, let T be thisArg; else let T be undefined.
+    if (arguments.length > 1) {
+      T = thisArg;
+    }
+
+    // 6. Let A be a new array created as if by the expression new Array(len) 
+    //    where Array is the standard built-in constructor with that name and 
+    //    len is the value of len.
+    A = new Array(len);
+
+    // 7. Let k be 0
+    k = 0;
+
+    // 8. Repeat, while k < len
+    while (k < len) {
+
+      var kValue, mappedValue;
+
+      // a. Let Pk be ToString(k).
+      //   This is implicit for LHS operands of the in operator
+      // b. Let kPresent be the result of calling the HasProperty internal 
+      //    method of O with argument Pk.
+      //   This step can be combined with c
+      // c. If kPresent is true, then
+      if (k in O) {
+
+        // i. Let kValue be the result of calling the Get internal 
+        //    method of O with argument Pk.
+        kValue = O[k];
+
+        // ii. Let mappedValue be the result of calling the Call internal 
+        //     method of callback with T as the this value and argument 
+        //     list containing kValue, k, and O.
+        mappedValue = callback.call(T, kValue, k, O);
+
+        // iii. Call the DefineOwnProperty internal method of A with arguments
+        // Pk, Property Descriptor
+        // { Value: mappedValue,
+        //   Writable: true,
+        //   Enumerable: true,
+        //   Configurable: true },
+        // and false.
+
+        // In browsers that support Object.defineProperty, use the following:
+        // Object.defineProperty(A, k, {
+        //   value: mappedValue,
+        //   writable: true,
+        //   enumerable: true,
+        //   configurable: true
+        // });
+
+        // For best browser support, use the following:
+        A[k] = mappedValue;
+      }
+      // d. Increase k by 1.
+      k++;
+    }
+
+    // 9. return A
+    return A;
+  };
+}
+
 //-----------------------------------------------------------------------------
 
 var DEFAULT_WEIGHT = 0.1;
@@ -466,6 +556,8 @@ function executeLatBuilder() {
 		$('#results').slideUp();
 		$('#search-button').show();
 		$('#abort-button').hide();
+		$('.with-results').hide();
+		$('.without-results').show();
         },
         success: function(response) {
             if (response.error) {
@@ -475,6 +567,8 @@ function executeLatBuilder() {
 		$('#results').slideUp();
 		$('#search-button').show();
 		$('#abort-button').hide();
+		$('.with-results').hide();
+		$('.without-results').show();
             }
             else {
                 $('#status')
@@ -488,6 +582,8 @@ function executeLatBuilder() {
 		$('#results').slideDown();
 		$('#search-button').show();
 		$('#abort-button').hide();
+		$('.with-results').show();
+		$('.without-results').hide();
 		$('html, body').animate({scrollTop: $("#results").offset().top}, 600);
             }
         }
@@ -632,6 +728,16 @@ $('document').ready(function() {
 	else {
 	    $('#construction-gen').parent().slideUp();
 	}
+    });
+    $('a[href$="#gen-from-results"]').on('click', function(e) {
+	e.preventDefault();
+	var self = $(this);
+	var res = $('#results-gen').text().split(',').map(function(x) { return x.trim(); });
+	var gen = $('#construction-gen').inputArrayValues();
+	for (var i = 0; i < Math.min(gen.length, res.length); i++) {
+	    gen[i] = res[i];
+	}
+	$('#construction-gen').inputArrayValues(gen);
     });
 
     // validation
