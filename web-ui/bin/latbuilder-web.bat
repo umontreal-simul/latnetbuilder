5c9d5965da4b8afb820f909e9e6e53bded2f3d82
David Munger <mungerd@gmail.com>
2015-12-07 10:32:30 -0500

    web-ui: add latbuilder-web.bat

---
+++ web-ui/bin/latbuilder-web.bat
@@ -0,0 +1,26 @@
+@echo off
+
+rem   first, try with Python 3
+
+if exist %SystemRoot%\py.exe (
+  start %SystemRoot%\py.exe latbuilder-web.py
+  exit
+)
+
+rem   check if Python 2.7 is installed
+set PYIK="HKLM\SOFTWARE\Python\PythonCore\2.7\InstallPath"
+reg query %PYIK% /ve
+if errorlevel 1 (
+  echo Cannot find Python 2.7.  Please install from https://www.python.org/downloads/.
+  pause
+  exit 1
+)
+
+rem   extract Python installation path from registry
+for /f "skip=2 tokens=1,* delims=)" %%a in ('reg.exe query %PYIK% /ve') do (
+  for /f "tokens=1,*" %%c in ("%%b") do (
+    set pydir=%%d
+  )
+)
+
+start %pydir%/python.exe latbuilder-web.py
