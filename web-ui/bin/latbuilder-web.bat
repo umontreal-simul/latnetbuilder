d540765333c4d6d40ee3d1c7f1a9d13ab9621085
David Munger <mungerd@gmail.com>
2016-02-24 09:33:19 -0500

    cosmetic changes to latbuilder-web.bat

--- web-ui/bin/latbuilder-web.bat
+++ web-ui/bin/latbuilder-web.bat
@@ -11,16 +11,14 @@ rem   check if Python 2.7 is installed
 set PYIK="HKLM\SOFTWARE\Python\PythonCore\2.7\InstallPath"
 reg query %PYIK% /ve
 if errorlevel 1 (
-  if exist C:\Python27 (
+  if exist C:\Python27\python.exe (
     set pydir=C:\Python27
-  )
-  else (
+  ) else (
     echo Cannot find Python 2.7.  Please install from https://www.python.org/downloads/.
     pause
     exit 1
   )
-)
-else (
+) else (
   rem   extract Python installation path from registry
   for /f "skip=2 tokens=1,* delims=)" %%a in ('reg.exe query %PYIK% /ve') do (
     for /f "tokens=1,*" %%c in ("%%b") do (
