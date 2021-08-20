39509d053a0ef14cd71551ab63d7112f0df536a8
Pierre Marion <pierre23.marion@free.fr>
2021-08-20 10:05:53 +0200

    Parse point set description from a file in CLI.
    This commit adds this possibility both for lattices and nets.
    The point set description in the CLI has to be replaced by
    "file:path"
    where path stands for the absolute or relative path to the file.

--- include/netbuilder/Parser/ExplorationMethodParser.h
+++ include/netbuilder/Parser/ExplorationMethodParser.h
@@ -21,6 +21,7 @@
 #include <boost/algorithm/string/split.hpp>
 #include <boost/algorithm/string/classification.hpp>
 #include <boost/lexical_cast.hpp>
+#include <fstream>
 
 #include "netbuilder/Types.h"
 #include "netbuilder/Parser/NetDescriptionParser.h"
@@ -74,11 +75,21 @@ struct ExplorationMethodParser
         unsigned int r = 0;
 
         if (name == "evaluation"){
-            if (explorationDescriptionStrings.size() < 2)
+            std::string netDescritionString;
+            if (explorationDescriptionStrings.size() < 2 || explorationDescriptionStrings.size() > 3)
             {
-                throw BadExplorationMethod("net description must be specified; see --help");
+                throw BadExplorationMethod("net description is not correctly specified; see --help");
             }
-            auto genValues = NetDescriptionParser<NC,ET>::parse(commandLine, explorationDescriptionStrings[1]);
+            else if (explorationDescriptionStrings.size() == 2){
+                netDescritionString = explorationDescriptionStrings[1];
+            }
+            else if (explorationDescriptionStrings.size() == 3){
+                std::ifstream t(explorationDescriptionStrings[2]);
+                std::stringstream buffer;
+                buffer << t.rdbuf();
+                netDescritionString = buffer.str();
+            }
+            auto genValues = NetDescriptionParser<NC,ET>::parse(commandLine, netDescritionString);
             auto net = std::make_unique<DigitalNet<NC>>(commandLine.m_dimension, commandLine.m_sizeParameter, std::move(genValues));
             return std::make_unique<Task::Eval>(std::move(net), std::move(commandLine.m_figure), commandLine.m_verbose);
         }
